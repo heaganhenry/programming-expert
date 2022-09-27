03324f77c68b9fa99c806a26918f8d133ebdd2f1
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-09-26 20:29:32 -0400

    Refactor solution

--- tournament-game-generator/main.py
+++ tournament-game-generator/main.py
@@ -1,52 +1,60 @@
 def get_number_of_teams():
-    num_teams = int(input("Enter the number of teams in the tournament: "))
-    while num_teams < 2:
-        print("The minimum number of teams is 2, try again.")
+    while True:
         num_teams = int(input("Enter the number of teams in the tournament: "))
+        if num_teams >= 2:
+            break
+        print("The minimum number of teams is 2, try again.")
 
     return num_teams
 
 def get_team_names(num_teams):
     team_names = []
+
     for num in range(num_teams):
-        team_name = input(f"Enter the name for team #{num + 1}: ")
-        while len(team_name) < 2 or len(team_name.split(" ")) > 2:
+        while True:
+            team_name = input(f"Enter the name for team #{num + 1}: ")
             if len(team_name.split(" ")) > 2:
                 print("Team names may have at most 2 words, try again.")
-            else:
+            elif len(team_name) < 2:
                 print("Team names must have at least 2 characters, try again.")
-            team_name = input(f"Enter the name for team #{num + 1}: ")
+            else:
+                break
+
         team_names.append(team_name)
 
     return team_names
 
 def get_number_of_games_played(num_teams):
-    games_played = int(input("Enter the number of games played by each team: "))
-    while games_played < num_teams - 1:
-        print("Invalid number of games. Each team plays each other at least once in the regular season, try again.")
+    while True:
         games_played = int(input("Enter the number of games played by each team: "))
+        if games_played >= num_teams - 1:
+            break
+        print("Invalid number of games. Each team plays each other at least once in the regular season, try again.")
 
     return games_played
 
 def get_team_wins(team_names, games_played):
     team_wins = []
-    for name in team_names:
-        num_wins = int(input(f"Enter the number of wins {name} had: "))
-        while num_wins < 0 or num_wins > games_played:
-            if num_wins < 0:
+
+    for team in team_names:
+        while True:
+            wins = int(input(f"Enter the number of wins {team} had: "))
+            if wins < 0:
                 print("The minimum number of wins is 0, try again.")
-            else:
+            elif wins > games_played:
                 print(f"The maximum number of wins is {games_played}, try again.")
-            num_wins = int(input(f"Enter the number of wins {name} had: "))
-        team_wins.append((name, num_wins))
+            else:
+                break
+
+        team_wins.append((team, wins))
 
     return team_wins
 
-def second_index_sort(item):
+def get_second_item(item):
     return item[1]
 
 def generate_home_and_away(team_wins):
-    sorted_wins = sorted(team_wins, key=second_index_sort)
+    sorted_wins = sorted(team_wins, key=get_second_item)
     for idx in range(num_teams // 2):
         home_team = sorted_wins[idx]
         away_team = sorted_wins[-1 - idx]
